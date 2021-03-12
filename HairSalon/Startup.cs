05b0e13240465341783476155e8a174af20e3ee4
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-12 08:47:29 -0800

    add init files

---
+++ HairSalon/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList
+{
+  public class Startup
+  {
+    public Startup(IWebHostEnvironment env)
+    {
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json"); //this line replaces .AddEnvironmentVariables();
+    Configuration = builder.Build();
+    }
+    public IConfigurationRoot Configuration { get; set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+    services.AddMvc();
+
+    //New code
+    services.AddEntityFrameworkMySql()
+        .AddDbContext<ToDoListContext>(options => options
+        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+}
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseDeveloperExceptionPage();
+      app.UseRouting();
+
+      app.UseEndpoints(routes =>
+      {
+        routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+      });
+
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("Hello World!");
+      });
+    }
+  }
+
+
+}
