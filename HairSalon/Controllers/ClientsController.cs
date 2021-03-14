d6e7f0af25753de683c3af707b74eb218ee87fc1
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-13 18:31:18 -0800

    add client GET/POST create() and corrersponding view

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -21,5 +21,20 @@ namespace HairSalon.Controllers
       List<Client> model = _db.Clients.Include(client => client.Stylist).ToList(); 
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+    ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+    return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+    _db.Clients.Add(client);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+    }
+
   }
 }
