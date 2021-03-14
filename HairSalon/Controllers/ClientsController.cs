7e558f2fc57d62343723f5c2541d03f121cd2975
Olha Hizhytska <olgainfotech@gmail.com>
2021-03-14 14:11:50 -0700

    add GET/POST requests delete client

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -57,7 +57,19 @@ namespace HairSalon.Controllers
     return RedirectToAction("Index");
     }
 
-    
+    public ActionResult Delete(int id)
+    {
+    var thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+    return View(thisClient);
+    }
 
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+    var thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+    _db.Clients.Remove(thisClient);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+    }
   }
 }
